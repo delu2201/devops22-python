81e9550553c58bbcd53dee17f9e76de811783398
Dennis Lunnelid <dennis.lunnelid@gmail.com>
2022-09-09 11:45:05 +0200

    sparar igen

--- lesson_5/delu2201/Excercise_new5.py
+++ lesson_5/delu2201/Excercise_new5.py
@@ -1,6 +1,3 @@
-from audioop import reverse
-
-
 firstname = "john"
 lastname = "smith"
 tele = "00468123456789"
@@ -16,7 +13,9 @@ print(f' {len(fullname)} {len(firstname)} {len(lastname)}')
 print(f"{fullname}\n{tele}")
 
 print(fullname + " " + tele)
+
 print(f'{fullname} {tele}')
+
 print('{} {}'.format(fullname,  tele))
 #print(f'Ditt namn är {} och ditt nummer är {}, fullname, tele')
 
@@ -26,3 +25,43 @@ print(fullname[1:-1])
 print(fullname.upper()[0:-1:2])
 print(fullname[::-1])
 print(fullname[5:5])
+
+#3
+price_of_car = int(input("what's the cost of the car? "))
+
+price_symbol = '${}'.format(price_of_car)
+
+print(price_symbol)
+
+if price_of_car > 50000:
+    print("\U0001f600")
+else:
+    print("\U0001F923")
+
+#4
+salery = 20000
+print(f"your current salary is {salery}. How much more are you asking for?")
+annual_raise = int(input("Enter amount: "))
+new_salary = salery + annual_raise
+perc_increase = (new_salary * 100 / salery) - 100
+count = 0
+
+while count < 5:
+        if count == 0:
+            print(f"You asked for a {perc_increase}% increase... NO! \U0001F923")
+            annual_raise = int(input("Try again!: "))
+            new_salary = salery + annual_raise
+            perc_increase = (new_salary * 100 / salery) - 100
+            count = count + 1
+        else:
+            for i in range(4):
+                count = count + 1
+                if perc_increase <= 5:
+                    print(f"Sure, you'll get {new_salary}!")
+                    break
+                else:    
+                    print(f"You asked for a {perc_increase}% increase... FUCK NO! \U0001F923")
+                    annual_raise = int(input("Try again!: "))
+                    new_salary = salery + annual_raise
+                    perc_increase = (new_salary * 100 / salery) - 100
+            break
